22e9e06a865cd24b8db20b96c5b18a57c2444e34
Pallagi Joe <pallagijoe@gmail.com>
2023-09-30 10:53:37 +0200

    async

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,36 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { TodoService, Todo } from '../services/todo.service';
 
 @Component({
-  selector: 'app-list',
-  templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+    selector: 'app-list',
+    templateUrl: './list.component.html',
+    styleUrls: ['./list.component.css'],
 })
 export class ListComponent implements OnInit {
-    tasks: Todo[] = [];
+    tasks: Observable<Todo[]> = this.todoService.getTasks();
 
     constructor(private todoService: TodoService) {}
 
-    ngOnInit(): void {
-        this.todoService.getTasks().subscribe(tasks => {
-            this.tasks = tasks;
-        });
-    }
+    ngOnInit(): void {}
 
     deleteTask(id: number): void {
-        this.todoService.deleteTaskById(id).subscribe(tasks => {
-            this.tasks = tasks;
-        });
+        this.tasks = this.todoService.deleteTaskById(id);
     }
 
     toggleIsDone(id: number): void {
-        const task = this.tasks.find(t => t.id === id);
-        if (task) {
-            task.isDone = !task.isDone;
-            
-            this.todoService.updateTask(task).subscribe(tasks => {
-                this.tasks = tasks;
-            });
-        }
-    }    
+        // const task = this.tasks.find(t => t.id === id);
+        // if (task) {
+        //     task.isDone = !task.isDone;
+        //     this.todoService.updateTask(task).subscribe(tasks => {
+        //         this.tasks = tasks;
+        //     });
+        // }
+    }
 }
